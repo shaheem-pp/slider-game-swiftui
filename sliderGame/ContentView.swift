82fd1a0ccee2d8668f6860ee9c4cd1281605c13d
Shaheem PP <shanofficial2000@gmail.com>
2024-07-11 15:19:47 -0400

    Initial Commit

---
+++ sliderGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sliderGame
+//
+//  Created by Shaheem PP on 11/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sliderGame/sliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  sliderGameApp.swift
+//  sliderGame
+//
+//  Created by Shaheem PP on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct sliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
